8365113b362feb242525dc6673982d42005484be
jinho9482 <157558300+jinho9482@users.noreply.github.com>
2024-05-08 11:42:32 +0900

    first commit

---
+++ auth/src/main/java/com/example/auth/service/AuthServiceImpl.java
@@ -0,0 +1,39 @@
+package com.example.auth.service;
+
+import com.example.auth.domain.entity.User;
+import com.example.auth.domain.entity.UserRepository;
+import com.example.auth.domain.request.SignInRequest;
+import com.example.auth.domain.request.SignUpRequest;
+import com.example.auth.domain.response.SignInResponse;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder; // new BCryptPasswordEncoder() 와 같다.
+
+    @Override
+//    @Transactional
+    public void insertUser(SignUpRequest signUpRequest) {
+        // 1. 유저가 있는지 찾아보고.
+        Optional<User> byId = userRepository.findByEmail(signUpRequest.email());
+        // 2-1 있으면 에러
+        if (byId.isPresent()) throw new IllegalArgumentException("해당 이메일로 가입된 사용자가 이미 존재합니다.");
+        // 2-2 없으면 insert
+        String encodedPassword = passwordEncoder.encode(signUpRequest.password());
+        User user = signUpRequest.toEntity(encodedPassword);
+        userRepository.save(user);
+    }
+
+}
---
+++ auth/src/test/java/com/example/auth/service/AuthServiceTest.java
@@ -0,0 +1,66 @@
+package com.example.auth.service;
+
+import com.example.auth.domain.entity.User;
+import com.example.auth.domain.entity.UserRepository;
+import com.example.auth.domain.request.SignUpRequest;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class AuthServiceTest {
+
+    @Autowired
+    private AuthService authService; // impl에 @Service가 없으면 bean 에 등록이 안되어있기 때문에 에러가 뜬다.
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Nested // 하나로 묶어서 테스트하는 것
+    class signUp {
+        @Test
+        void success() {
+            // given
+            SignUpRequest request = new SignUpRequest("wlshzz@naver.com",
+                    "1234",
+                    "머리좀긴크리링",
+                    LocalDate.of(2024, 5, 8),
+                    "male"
+            );
+
+            // when
+            authService.insertUser(request);
+            // then
+            Optional<User> byEmail = userRepository.findByEmail(request.email());
+            assertTrue(byEmail.isPresent());
+            assertNotEquals("1234", byEmail.get().getEmail());
+        }
+
+        @Test
+        void emailAlreadyExist() {
+            // given
+            SignUpRequest request = new SignUpRequest("wlshzz@naver.com",
+                    "1234",
+                    "머리좀긴크리링",
+                    LocalDate.of(2024, 5, 8),
+                    "male"
+            );
+
+            userRepository.save(User.builder().email("wlshzz@naver.com").build());
+            // when & then (데이터를 넣고 에러가 즉각 발생하기 때문에 같이 쓴다.)
+            authService.insertUser(request);
+            assertThrows(IllegalArgumentException.class, () -> {
+                authService.insertUser(request);
+            });
+
+        }
+    }
+}
