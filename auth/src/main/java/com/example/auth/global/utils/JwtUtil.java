72b056d649084a7dfec70817ee73c4636f7d9bf7
jinho9482 <157558300+jinho9482@users.noreply.github.com>
2024-05-08 12:14:19 +0900

    [set] environment variable setting

--- auth/src/main/java/com/example/auth/global/utils/JwtUtil.java
+++ auth/src/main/java/com/example/auth/global/utils/JwtUtil.java
@@ -1,2 +1,16 @@
-package com.example.auth.global.utils;public class JwtUtil {
+package com.example.auth.global.utils;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}") // 환경 변수에 있는 것을 쓰기 위함
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+
 }
