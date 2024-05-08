6c0eead1abc86bcb992ea11600d0bf4a90904bf2
jinho9482 <157558300+jinho9482@users.noreply.github.com>
2024-05-08 15:24:41 +0900

    [Feat] Signup, login controller

--- auth/src/main/java/com/example/auth/controller/AuthController.java
+++ auth/src/main/java/com/example/auth/controller/AuthController.java
@@ -1,11 +1,27 @@
 package com.example.auth.controller;
 
+import com.example.auth.domain.request.SignInRequest;
+import com.example.auth.domain.request.SignUpRequest;
+import com.example.auth.domain.response.SignInResponse;
 import com.example.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/auths")
 public class AuthController {
     private final AuthService authService;
+
+    @PostMapping("/signup")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void signUp(@RequestBody SignUpRequest req) {
+        authService.insertUser(req);
+    }
+
+    @PostMapping("/signin")
+    public SignInResponse signIn(@RequestBody SignInRequest req) {
+        return authService.signIn(req);
+    }
 }
